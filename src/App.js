bace330961bc82c6c2d0cca703b61b3a46db303b
veerabhadraswamy <jarviscommand3@gmail.com>
2023-07-07 06:40:54 +0000

    App Routes Added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,16 @@
+import {Route, Switch, Redirect} from 'react-router-dom'
+import Home from './components/Home'
+import Login from './components/Login'
+import NotFound from './components/NotFound'
 import './App.css'
 
-// Replace your code here
-const App = () => <h1>Hello World!</h1>
+const App = () => (
+  <Switch>
+    <Route path="/" exact component={Home} />
+    <Route path="/ebank/login" exact component={Login} />
+    <Route path="/not-found" exact component={NotFound} />
+    <Redirect to="/not-found" />
+  </Switch>
+)
+
+export default App
