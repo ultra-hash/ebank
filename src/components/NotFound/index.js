6a06e346d7b84071410e3fb21e1a653bf13d2d8c
veerabhadraswamy <jarviscommand3@gmail.com>
2023-07-07 07:11:48 +0000

    implemented NotFound component

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,3 +1,24 @@
-const NotFound = () => {}
+import {
+  OuterContainer,
+  InnerContainer,
+  NotFoundImg,
+  Heading,
+  Paragraph,
+} from './styledComponents'
+
+const NotFound = () => (
+  <OuterContainer>
+    <InnerContainer>
+      <NotFoundImg
+        src="https://assets.ccbp.in/frontend/react-js/ebank-not-found-img.png"
+        alt="not found"
+      />
+      <Heading>Page Not Found</Heading>
+      <Paragraph>
+        We are sorry, the page you requested could not be found.
+      </Paragraph>
+    </InnerContainer>
+  </OuterContainer>
+)
 
 export default NotFound
---
+++ src/components/NotFound/styledComponents.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+
+export const OuterContainer = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`
+export const InnerContainer = styled.div`
+  flex-grow: 1;
+  width: 100%;
+  max-width: 1000px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+export const NotFoundImg = styled.img`
+  width: 50%;
+`
+export const Heading = styled.h1`
+  color: #183b56;
+`
+export const Paragraph = styled.p`
+  color: #5a7184;
+`
